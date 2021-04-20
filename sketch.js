bc01c07b25bbc6cc6287612fb721886f0479eeb7
vatsalstar <74692621+vatsalstar@users.noreply.github.com>
2021-04-20 20:27:27 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,53 +1,66 @@
-hr = hour();
-mn = munite();
-sc = second();
+var hr, mn, sc;
+var hrAngle, mnAngle, scAngle; 
 
-function setup() {
-  createCanvas(800,400);
-  createSprite(400, 200, 50, 50);
+function setup(){
+    createCanvas(400,400); 
+    //To use angle in Degrees
+    angleMode(DEGREES);   
 }
 
-function draw() {
-  background(255,255,255);  
+function draw(){
+    background(0);
 
-scAngle = (0.60,0,360);
+    //Translation and rotation 
+    translate(200,200)
+    rotate(-90)
 
-  drawSprites();
+    //Calculating time using predefined func from p5.js
+    hr = hour();
+    mn = minute();
+    sc = second();
 
-}
+    //To create ab iterative rotation
+    scAngle = map(sc, 0, 60, 0, 360);
+    mnAngle = map(mn,0,60,0,360)
+    hrAngle = map(hr % 12,0,12,0,360)
 
+    //drawing seconds hand
+    push();
+    rotate(scAngle); //rotate the hand based on angle calculated
+    stroke(255,0,0);
+    strokeWeight(7);
+    line(0,0,100,0);
+    pop()
 
-function Translate(){
+    //drawing mins hand
+    push();
+    rotate(mnAngle);
+    stroke(0,255,0);
+    strokeWeight(7);
+    line(0,0,75,0);
+    pop()
 
-arc(50, 55, 50, 50, 0, HALF_PI);
-noFill();
-arc(50, 55, 60, 60, HALF_PI, PI);
-arc(50, 55, 70, 70, PI, PI + QUARTER_PI);
-arc(50, 55, 80, 80, PI + QUARTER_PI, TWO_PI);
+    //drawing hr hand
+    push();
+    rotate(hrAngle);
+    stroke(0,0,255);
+    strokeWeight(7);
+    line(0,0,50,0);
+    pop()
 
-arc(50, 50, 80, 80, 0, PI + QUARTER_PI);
+    stroke(255,0,255);
+    point(0,0)
 
-arc(50, 50, 80, 80, 0, PI + QUARTER_PI, OPEN);
-
-arc(50, 50, 80, 80, 0, PI + QUARTER_PI, CHORD);
-
-arc(50, 50, 80, 80, 0, PI + QUARTER_PI, PIE);
-
-
-stroke(255,0,0);
-strokeWeight(7);
-line(0,0,100,0);
-}
-
-
-function Map(){
-
-  push()
-  rotate(scAngle);
-stroke(255,0,0);
-strokeWeight(7);
-line(0,0,100,0);
-pop();
-
-
+    //drawing the arcs
+    strokeWeight(10);
+    noFill();
+    //Seconds
+    stroke(255,0,0);
+    arc(0,0,300,300,0,scAngle);
+    //Minutes
+    stroke(0,255,0);
+    arc(0,0,280,280,0,mnAngle);
+    //Hour
+    stroke(0,0,255);
+    arc(0,0,260,260,0,hrAngle);
 }
